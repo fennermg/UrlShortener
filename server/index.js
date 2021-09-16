a7f12402f60dc9497a447be47da707539dc2f45d
fennermg <fmenagaray@gmail.com>
2021-09-16 14:31:34 -0600

    Connection to database created

--- server/index.js
+++ server/index.js
@@ -1,9 +1,13 @@
 const express = require("express");
+const connectDB = require('./config/db');
 
 const app = express();
+
+connectDB();
 
 app.use(express.json({ extented: false }));
 
 const PORT = 5000;
 
 app.listen(PORT, ()=>{console.log(`Server running on port ${PORT}`)});
+
